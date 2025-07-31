fd65ce1313643c763f778de8852e70c142808410
Abhi-to-code <114558252+Abhi-to-code@users.noreply.github.com>
2025-08-01 02:04:31 +0530

    Update uart_protocol.v

--- uart_protocol.v
+++ uart_protocol.v
@@ -24,26 +24,35 @@ module uart_protocol (
     input clk,
     input rst,
     output reg [7:0] data_from_memory,
-    output reg [7:0] data_sent,
+    output reg [12:0] data_sent,
     output reg line,
     output reg baud_tick,
-    output reg [7:0] data_recorded,
-    output reg [7:0] data_saved_to_memory
+    output reg [12:0] data_recorded,
+    output reg [7:0] data_saved_to_memory,
+    output reg error_detected,
+    output reg error_corrected,
+    output reg overall_parity_error
 );
 
 
 wire [7:0] data_bus;
 wire [7:0] data_bus_out;
 wire [7:0] data_memory_test;
-wire frame;
+wire frame, frame_out;
 wire ready;
 wire ready_rx;
+wire error_bit;
+wire wire_error_detected;
+wire wire_error_corrected;
+wire wire_overall_error;
 
 memory_1 transmitter_memory(clk, ready, rst, data_bus);
 
 uart_tx #(10) transmitter(clk, data_bus, rst, ready, frame);
 
-uart_rx #(10) receiver(clk, rst, frame, data_bus_out, ready_rx);
+channel #(3) bi_syn_ch(clk, frame, 1, frame_out);
+
+uart_rx #(10) receiver(clk, rst, frame_out, data_bus_out, ready_rx, wire_error_detected, wire_error_corrected, wire_overall_error);
 
 memory_2 receiver_memory(clk, rst, data_bus_out, ready_rx, data_memory_test);
 
@@ -52,8 +61,12 @@ always @(posedge clk) begin
     data_sent <= transmitter.data_register;
     line <= frame;
     baud_tick <= transmitter.baud_tick;
-    data_recorded <= data_bus_out;
+    data_recorded <= receiver.reg_for_testing;
+    //data_recorded <= receiver.reg_for_testing
     data_saved_to_memory <= data_memory_test;
+    error_corrected <= wire_error_corrected;
+    error_detected <= wire_error_detected;
+    overall_parity_error <= wire_overall_error;
 end
 
 
